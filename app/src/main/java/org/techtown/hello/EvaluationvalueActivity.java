5d89087a48eaad37c62093714d767cf26c05e08c
calmalong <infiniteleo7@gmail.com>
2023-04-18 00:24:54 +0900

    니코틴중독평가페이지

---
+++ app/src/main/java/org/techtown/hello/EvaluationvalueActivity.java
@@ -0,0 +1,44 @@
+package org.techtown.hello;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class EvaluationvalueActivity extends Activity {
+
+    Button btnGather;
+    TextView textFinalScore;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.evaluation_result);
+
+
+        textFinalScore = (TextView) findViewById(R.id.textFinalScore);
+        btnGather = (Button) findViewById(R.id.btnGather);
+
+
+        Bundle bundle = getIntent().getExtras();
+        String text1 = bundle.getString("score");
+
+        textFinalScore.setText("당신의 니코틴의존도는 " + text1 +"점 입니다.");
+
+
+
+        btnGather.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), UserinfoActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
