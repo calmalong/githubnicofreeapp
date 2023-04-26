6bff44af25fc1c968fe05d8e45722b5c8d7b059e
calmalong <infiniteleo7@gmail.com>
2023-04-26 15:04:28 +0900

    사용자흡연정보수집,계획서

--- app/src/main/java/org/techtown/hello/PlanActivity.java
+++ app/src/main/java/org/techtown/hello/PlanActivity.java
@@ -3,6 +3,7 @@ package org.techtown.hello;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.RadioButton;
 import android.widget.TextView;
@@ -11,15 +12,18 @@ import androidx.annotation.Nullable;
 
 public class PlanActivity extends Activity {
 
+    Button btnDate;
     RadioButton rdoDate;
     DatePicker dPicker;
     TextView tvYear, tvMonth, tvDay;
     int selectYear, selectMonth, selectDate;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.plan);
+
+        btnDate = (Button) findViewById(R.id.btnDate);
 
         rdoDate = (RadioButton) findViewById(R.id.rdoDate);
 
@@ -36,6 +40,24 @@ public class PlanActivity extends Activity {
             public void onClick(View v) {
                 dPicker.setVisibility(View.VISIBLE);
             }
+        });
+
+        dPicker.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                selectYear = year;
+                selectMonth = monthOfYear+1;
+                selectDate = dayOfMonth;
+            }
+        });
+
+        btnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dPicker.setVisibility(View.INVISIBLE);
+                tvYear.setText(Integer.toString(selectYear));
+                tvMonth.setText(Integer.toString(selectMonth));
+                tvDay.setText(Integer.toString(selectDate));}
         });
     }
 
